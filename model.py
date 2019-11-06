10f64befec49c056ae9ad83a1faebd387b14a098
Eoghan O'Connor <oconnorjohn40@gmail.com>
2019-11-06 19:58:32 +0000

    Adding imports and importing mnist dataset from keras api

---
+++ model.py
@@ -0,0 +1,14 @@
+# imports
+# NumPy is a library for the Python
+# programming language, adding support for large,
+# multi-dimensional arrays and matrices
+import numpy as np
+# Matplot lib is a 2D plotting library for Python
+import matplotlib.pyplot as plt
+# Keras is a hign level open-source nerual-network library written in Python
+# it runs on top of TensorFlow, Theano and PlaidML
+import keras as kr
+# The MNIST database is a large database of handwritten digits that is
+# commonly used for training various image processing systems.
+# This imports The MNIST dirctly from the from the keras API
+from keras.datasets import mnist
