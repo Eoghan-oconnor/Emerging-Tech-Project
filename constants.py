5001862b2a9ff423d0da55c25499aa78d3201eb4
Eoghan O'Connor <oconnorjohn40@gmail.com>
2019-11-06 20:30:25 +0000

    Adding constants.py and assigning mnist dataset to variables

---
+++ constants.py
@@ -0,0 +1,3 @@
+# This file will contain Constants
+img_width = 28
+img_height = 28
--- model.py
+++ model.py
@@ -12,3 +12,15 @@ import keras as kr
 # commonly used for training various image processing systems.
 # This imports The MNIST dirctly from the from the keras API
 from keras.datasets import mnist
+# Importing the constants.py
+import constants
+
+# This downloads the MNIST dataset from the Keras API. The dataset has 60,000
+# images and associated labels used for training and 10,000 testing images
+# with associated labels. We need to seperate the dataset into two groups
+# a training group and a testing group. train_imgs, train_labels,
+# test_imgs and test_label.
+(train_imgs, train_labels), (test_imgs, test_label) = mnist.load_data()
+
+
+
