8e484a341cec503a7c4c15c3f49241e6b263c313
Eoghan O'Connor <oconnorjohn40@gmail.com>
2019-12-01 15:10:41 +0000

    Changing the way the MNIST dataset is accessed, Adding dataset gzip files

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 # This file will contain Constants
 img_width = 28
 img_height = 28
-no_of_numbers = 10
-rate = 0.7
+num_classes = 10
+rate = 0.5
 batch_size = 128
 num_epoch = 20
--- model.py
+++ model.py
@@ -27,48 +27,100 @@ import constants
 from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
 
 from keras.models import load_model
+import gzip
 
 
 # This downloads the MNIST dataset from the Keras API. The dataset has 60,000
 # images and associated labels used for training and 10,000 testing images
 # with associated labels. We need to seperate the dataset into two groups
 # a training group and a testing group. train_imgs, train_labels,
-# test_imgs and test_label.
-(train_imgs, train_labels), (test_imgs, test_label) = mnist.load_data()
+# test_imgs and test_labels.
+(train_imgs, train_labels), (test_imgs, test_labels) = mnist.load_data()
 
 # We have to reshape the MNIST dataset with Keras, we will convert it from a 3d
 # Array to a 4d NumPy array
 # Making sure train_imgs and test_imgs are floats, so we can use decimal points
-train_imgs = train_imgs.reshape(train_imgs.shape[0], constants.img_width,
-                                constants.img_height, 1)
-test_imgs = test_imgs.reshape(test_imgs.shape[0], constants.img_width,
-                              constants.img_height, 1)
-input_shape = (constants.img_width, constants.img_height, 1)
+train_imgs = train_imgs.reshape(train_imgs.shape[0], 1, constants.img_width,
+                                constants.img_height)
+test_imgs = test_imgs.reshape(test_imgs.shape[0], 1, constants.img_width,
+                              constants.img_height)
+input_shape = (1, constants.img_width, constants.img_height)
 train_imgs = train_imgs.astype('float32')
 test_imgs = train_imgs.astype('float32')
 
-# Data is normalized when being used in a nerual network t obtain a mean close
+
+
+# Data is normalized when being used in a nerual network to obtain a mean close
 # to 0 Normalizing the data generally speeds up learning and
 # leads to faster convergence. Normalizing the RGB code by dividing it by the
 # max RGB value(255)
 train_imgs /= 255
 test_imgs /= 255
 
-train_labels = kr.utils.to_categorical(train_labels, constants.no_of_numbers)
-test_label = kr.utils.to_categorical(test_label, constants.no_of_numbers)
+num_classes = 10
+
+train_labels = kr.utils.to_categorical(train_labels, num_classes)
+test_labels = kr.utils.to_categorical(test_labels, num_classes)
+train_labels[0]
+
+print(train_imgs.shape)
+print(train_labels.shape)
+print(test_imgs.shape)
+print(test_labels)
+
 
 # Creating a model and adding layers
 # Sequential allows you to create a nerual network layer by layer
 model = Sequential()
-
 model.add(Conv2D(32, kernel_size=(3, 3),
                  activation='relu', input_shape=input_shape))
 model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(4, 4)))
-model.add(Dropout(0.25))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(constants.rate))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(constants.no_of_numbers, activation='softmax'))
+model.add(Dense(num_classes, activation='softmax'))
 model.compile(loss=kr.losses.categorical_crossentropy,
               optimizer=kr.optimizers.Adadelta(), metrics=['accuracy'])
+
+
+# Current set up requires the model to be trained every time the application
+# is ran, so we will train out file here and ave the model for future use.
+# The try and except block in Python is used to catch and handle exceptions.
+# Python executes code following the try statement as a “normal” part of the
+# program.The code that follows the except statement is the program's response
+# to any exceptions in the preceding try clause.
+
+print(test_imgs.shape)
+print(test_labels.shape)
+
+
+model.fit(train_imgs, train_labels,
+          batch_size=constants.batch_size,
+          epochs=constants.num_epoch,
+          verbose=1,
+          validation_data=(test_imgs, test_labels)
+          )
+model.save("model.h5")
+
+plt.imshow(test_imgs[3333].reshape(28, 28), cmap="gray")
+plt.show()
+
+try:
+    print("Model loaded")
+    model = load_model("model.h5")
+except IndexError:
+    print("Error opening file, no model Present")
+    print("Creating Model")
+    model_log = model.fit(train_imgs, train_labels,
+                          batch_size=constants.batch_size,
+                          epochs=constants.num_epoch,
+                          verbose=1,
+                          validation_data=(test_imgs, test_labels)
+                          )
+    model.save("model.h5")
+    print("Model Saved.")
+
+plt.imshow(test_imgs[3333].reshape(28, 28), cmap="gray")
+plt.show()
