c91d6c447c3dd5549171a26cd939d2380f322c29
Eoghan O'Connor <oconnorjohn40@gmail.com>
2019-12-10 17:08:04 +0000

    Canvas working and can be cleared by user

--- FlaskFrontEnd/Static/Javascript.js
+++ FlaskFrontEnd/Static/Javascript.js
@@ -1,38 +1,91 @@
-// console.log("Hello") Debug to make sure the js file was linking the Home.html
-
 // Resources : https://www.w3schools.com/graphics/canvas_drawing.asp
 //             http://www.williammalone.com/articles/create-html5-canvas-javascript-drawing-app/
 //             http://bencentra.com/code/2014/12/05/html5-canvas-touch-events.html?fbclid=IwAR0mCUF5uZYTQUk4zXebQCWWw6Iau31fEsGhRm_OaMvRGNysZ5piTqP4LHA
 
 // Variables
 // Setting up Canvas
-var canvas = document.getElementById("numberCanvas");
-var ctx = canvas.getContext("2d");
-ctx.strokeStyle = "#222222"
-ctx.lineWidth = 2;
 
-// Setting up mouse events
+// debug
+console.log("js file loaded");
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+ctx.strokeStyle = "#222222";
+ctx.lineWith = 2;
+
+// Setting up mouse event variables
 var drawing = false;
 var mousePosition = { x:0, y:0 };
 var lastPosition = mousePosition;
 
+
+//Debug
+//console.log("before listeners");
 // Captures mouse events
 canvas.addEventListener("mousedown", function (e) {
+    console.log("mousedown");
         drawing = true;
-    lastPos = getMousePos(canvas, e);
-    }, false);
-    canvas.addEventListener("mouseup", function (e) {
-    drawing = false;
-    }, false);
-    canvas.addEventListener("mousemove", function (e) {
-    mousePos = getMousePos(canvas, e);
-    }, false);
+  lastPosition = getMousePos(canvas, e);
+}, false);
+canvas.addEventListener("mouseup", function (e) {
+    console.log("mouseup");
+  drawing = false;
+}, false);
+canvas.addEventListener("mousemove", function (e) {
+    console.log("mouseup");
+  mousePosition = getMousePos(canvas, e);
+}, false);
 
-    // Get the position of the mouse relative to the canvas
-    function getMousePos(canvasDom, mouseEvent) {
-    var rect = canvasDom.getBoundingClientRect();
-    return {
+
+// == Get mouse position ==
+function getMousePos(canvasDom, mouseEvent) {
+  var rect = canvasDom.getBoundingClientRect();
+  return {
     x: mouseEvent.clientX - rect.left,
     y: mouseEvent.clientY - rect.top
-    };
+  };
 }
+
+
+
+// == Gives regular intervals for draw animation ==
+window.requestAnimFrame = (function (callback) {
+  return window.requestAnimationFrame || 
+     window.webkitRequestAnimationFrame ||
+     window.mozRequestAnimationFrame ||
+     window.oRequestAnimationFrame ||
+     window.msRequestAnimaitonFrame ||
+     function (callback) {
+        window.setTimeout(callback, 1000/60);
+     };
+})();
+
+
+// This function draws to the canvas
+function renderCanvas() {
+  if (drawing) {
+    ctx.moveTo(lastPosition.x, lastPosition.y);
+    ctx.lineTo(mousePosition.x, mousePosition.y);
+    ctx.stroke();
+    lastPosition = mousePosition;
+  }
+}
+
+// Allows Animation
+
+(function drawLoop () {
+  requestAnimFrame(drawLoop);
+  renderCanvas();
+})();
+
+$("#clearButton").click(function(e) {
+    clearTheCanvas();
+});
+
+function clearTheCanvas()[
+    ctx.clearRect(0,0,150,200);
+    document.getElementById("canvas").style.display = "none"
+]
+
+
+
+
